3e484387315289c64a26bcd0fef9fbe237e59da0
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-04-22 12:07:02 -0500

    completed further study

---
+++ static/js/about.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+function AboutPage(props) {
+    return (
+        <div>
+            <h2>Veronica Hall</h2>
+            <h4>Github: ver0nicahall</h4>
+        </div>
+    )
+}
+
+ReactDOM.render(<AboutPage />, document.querySelector('#about'))
